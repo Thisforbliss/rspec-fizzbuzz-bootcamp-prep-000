28dae242a6cfbf59bb68d546b1f67294c55fb348
Malcolm Leake <echelon1089@gmail.com>
2018-03-14 23:26:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,11 @@ def fizzbuzz(int)
 
  if int.to_i % 3&&5 == puts "FizzBuzz"
  
- elseif int.to_i % 3 == puts "Fizz"
+ elsif int.to_i % 3 == puts "Fizz"
  
- elseif int.to_i % 5 == puts "Buzz"  
+ elsif int.to_i % 5 == puts "Buzz"  
  
  else  puts "nil" 
    
-   3&&5 == puts "FizzBuzz"
-   3 == puts "Fizz"
-   5 == puts "Buzz"
-end
+
 end
